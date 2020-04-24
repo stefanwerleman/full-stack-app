6f6c85e7d59095ba8e56a8cb931606f388651be6
stefanwerleman <stefanwerleman@knights.ucf.edu>
2020-04-24 04:38:10 -0400

    Finished Redirecting Operations

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+const API_URL = "https://cfw-takehome.developers.workers.dev/api/variants";
+
 addEventListener('fetch', event =>
 {
-  event.respondWith(handleRequest(event.request))
+   event.respondWith(handleRequest(event.request));
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request)
 {
-  return new Response('Hello worker!',
-  {
-    headers: { 'content-type': 'text/plain' },
-  })
+   // Makes fetch request
+   const response = await fetch(API_URL);
+   const data = await response.json();
+
+   // Capturing array of URLS
+   var variants = data.variants;
+
+   // Randomly chose a URL
+   var index = Math.round(Math.random());
+   var url = variants[index];
+
+   // Returning new response
+   return Response.redirect(url);;
 }
